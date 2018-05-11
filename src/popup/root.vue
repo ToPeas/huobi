117d65478545ea18fade41801ef584951a92f97b
topeas <peiqixin@gmail.com>
2018-05-11 16:41:32 +0800

    feat:添加一个扩展的脚手架

---
+++ src/popup/root.vue
@@ -0,0 +1,23 @@
+<template >
+  <div>
+    <el-button type="primary" @click="tab">New tab</el-button>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({}),
+  computed: {},
+  created() {},
+  mounted() {},
+  methods: {
+    tab() {
+      chrome.tabs.create({ url: 'pages/app.html' })
+    }
+  }
+}
+</script>
+<style lang="scss">
+div {
+  color: blue;
+}
+</style>
